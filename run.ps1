28abc17aede90337ab0d16c337adfbf481fdf3bc
Zhenya <Canimore@yandex.ua>
2021-12-06 17:03:26 +0300

    end job

---
+++ run.ps1
@@ -0,0 +1,17 @@
+﻿$files = Get-ChildItem -Name -File
+
+foreach($file in $files)
+{
+    $filePath = Join-Path -ChildPath $file  -Path "."
+    $extension = $filePath.Split(".")[-1]
+    $folderPath = Join-Path -ChildPath $extension -Path "."
+    if(!(Test-Path -Path $folderPath))
+    {
+       New-Item -Path $folderPath -ItemType "directory"
+    }
+    if(Test-Path -Path $filePath)
+    {
+        Move-Item -Path $filePath -Destination $folderPath
+    }
+    
+}
